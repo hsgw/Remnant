c3e563be84bfcfa70981bf678e0a974ed3b0e5a7
hsgw <urkwtky@gmail.com>
2019-05-22 12:08:48 +0900

    add arduino src

---
+++ src/remnant/remnant.ino
@@ -0,0 +1,19 @@
+#include "remnant.h"
+
+const uint16_t PROGMEM keymap[SWITCHES_NUM] = 
+  LAYOUT( 
+    LCTL(KC_A), LCTRL(KC_C), KC_UP,       LCTRL(KC_V),
+    KC_LSFT,    KC_LEFT,     KC_DOWN,     KC_RIGHT
+  );
+
+Remnant remnant;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  remnant.update();
+}
